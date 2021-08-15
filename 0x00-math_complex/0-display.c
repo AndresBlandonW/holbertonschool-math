0961e1d7abea60c6e97f0795ade0e3bcdfa2e5a1
AndresBlandonW <andblandon@hotmail.com>
2021-08-14 18:24:24 -0700

    task 0

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -8,9 +8,30 @@
 void display_complex_number(complex c)
 {
 
+if (c.re == 0)
+{
+
+if (c.im == 0)
+printf("0\n");
+else
+printf("%.9g\n", c.im);
+
+}
+else
+{
+
 if (c.im == 0)
 printf("%.9g\n", c.re);
-else
+
+if (c.im == 1)
+printf("%.9g + i\n", c.re);
+
+if (c.im > 1)
 printf("%.9g + %.9gi\n", c.re, c.im);
+
+if (c.im < -1)
+printf("%.9g - %.9gi\n", c.re, c.im * (-1));
+
+}
 
 }
