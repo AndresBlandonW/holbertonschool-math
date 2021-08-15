2c803fddf08b8a3c0467296beef4c137b63a103c
AndresBlandonW <andblandon@hotmail.com>
2021-08-14 18:54:05 -0700

    task 0 update

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -13,8 +13,6 @@ if (c.re == 0)
 
 if (c.im == 0)
 printf("0\n");
-else if (c.im < 0)
-printf("- %.9gi\n", c.im * (-1));
 else
 printf("%.9g\n", c.im);
 
@@ -27,6 +25,9 @@ printf("%.9g\n", c.re);
 
 if (c.im == 1)
 printf("%.9g + i\n", c.re);
+
+if (c.im == -1)
+printf("%.9g - i\n", c.re);
 
 if (c.im > 1)
 printf("%.9g + %.9gi\n", c.re, c.im);
